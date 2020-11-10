5809e893a2ff1210e8428651b24ab92c59b7ba78
Edgar Corona <coronam.edgar@gmail.com>
2020-11-10 08:30:09 -0600

    App component: TS and HTML for app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { global_info } from './services/global_info';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forumAngular';
+  title = 'Personal Blog';
+  public userInfo;
+  public token;
+  public url;
+
+  constructor(public _userService: UserService,){
+    this.loadUser();
+    this.url = global_info.url;
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    console.log('Page loaded correctly');
+  }
+
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    this.loadUser();
+
+  }
+
+  loadUser(){
+    this.userInfo = this._userService.getInfoUser();
+    this.token = this._userService.getToken();
+  }
 }
