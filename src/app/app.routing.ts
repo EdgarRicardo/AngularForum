0ed46993a72d9032dbf13adfe6a414d93b197e54
Edgar Corona <coronam.edgar@gmail.com>
2020-11-10 08:32:31 -0600

    Routing: Routes file for the app

---
+++ src/app/app.routing.ts
@@ -0,0 +1,26 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+
+//Defining routes
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'logout/:sure', component:LoginComponent},
+  {path: 'signup', component:SignupComponent},
+  {path: 'home', component:HomeComponent},
+  {path: 'userSettings', component:UserEditComponent},
+  {path: '**', component:ErrorComponent},
+  {path: 'error', component:ErrorComponent}
+];
+
+  //{path: 'settings', component:UserEditComponent, canActivate: [IdentityGuard]}
+
+//Importing configuration
+export const routingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(routes);
+
